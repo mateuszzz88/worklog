519f858a9921c361fda8043643c98e2342b7b8ec
Mateusz Lewicki <m.lewicki@samsung.com>
2016-05-02 15:38:26 +0200

    fixed formatting

--- work_log.py
+++ work_log.py
@@ -3,19 +3,26 @@ import datetime
 import os
 import subprocess
 
-LOG_FILE = os.path.expanduser('~/hours_log.csv')
+LOG_FILE = os.path.expanduser('~/hours_log.txt')
 
 
 cmd = subprocess.Popen(["dbus-monitor \"type='signal',interface='org.freedesktop.ScreenSaver',path='/ScreenSaver'\""],
                        shell=True,
                        stdout=subprocess.PIPE)
 
-last_lock = last_unlock = when = datetime.datetime.now()
+last_lock = last_unlock = datetime.datetime.now()
 
 with open(LOG_FILE, 'a') as f:
-    f.write("\nlogger started on %s\n" % when.ctime())
+    f.write("\nlogger started on %s\n" % last_unlock.ctime())
 
 comment = None
+
+
+def timedelta_format(delta):
+    seconds = delta.total_seconds()
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    return '%d:%02d:%02d' % (hours, minutes, seconds)
 
 while True:
     line = cmd.stdout.readline().decode('utf-8')
@@ -26,26 +33,25 @@ while True:
         when_str = when.strftime("%Y-%m-%d %H:%M:%S")
 
         status = 'UNLOCK' if 'false' in line else 'LOCKED'
-        longpause = comment and diff > datetime.timedelta(0, 30 * 60, 0)
-        if longpause:
-            with open(LOG_FILE, 'a') as f:
-                f.write('\n')
 
         comment = None
-        if 'false' in line:
+        if status == 'UNLOCK':
             # screen unlocked
             last_unlock = when
-            if last_lock:
-                diff = when - last_lock
-                comment = " (locked for h:m:s = %s)" % str(diff)
+            diff = when - last_lock
+            comment = " (locked for h:m:s = %s)" % timedelta_format(diff)
         else:
             # screen locked
             last_lock = when
-            if last_unlock:
-                diff = when - last_lock
-                comment = " (unlocked for h:m:s = %s)" % str(diff)
-        if longpause:
+            diff = when - last_unlock
+            comment = " (unlocked for h:m:s = %s)" % timedelta_format(diff)
+
+        longpause = diff > datetime.timedelta(0, 30 * 60, 0)
+        if longpause and status == 'UNLOCK':
+            with open(LOG_FILE, 'a') as f:
+                f.write('\n')
             comment += " !!!"
+
         new_log = "{time} screen {status}{comment}\n".format(
             time=when_str,
             status=status,
@@ -54,5 +60,3 @@ while True:
         print(new_log)
         with open(LOG_FILE, 'a') as f:
             f.write(new_log)
-
-
